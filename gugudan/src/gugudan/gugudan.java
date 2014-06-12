4becb3ad254a622982ba136276da233b44b29fb3
KIMJUHYEON <kjh9137@gmail.com>
2014-06-12 15:25:24 +0900

    github test

---
+++ gugudan/src/gugudan/gugudan.java
@@ -0,0 +1,18 @@
+package gugudan;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i + "dan");
+			System.out.println("==========");
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i + " X " + j + " : " + (i * j));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
